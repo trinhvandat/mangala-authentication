de19b9a200d6f168e07dc0f91c93551bc1e35486
Trịnh Văn Đạt <53535112+trinhvandat@users.noreply.github.com>
2026-02-03 15:42:35 +0700

    Add code base with java 25 and spring 4.x (#1)

---
+++ src/main/java/org/mangala/authentication/MainApplication.java
@@ -0,0 +1,11 @@
+package org.mangala.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
